7ec9bb32514a31ffd543a63924ec3e0bdf39428c
MarksPro <marcosfsprivado@gmail.com>
2019-06-08 23:06:59 -0300

    Compilador Sass, auto prefixer, concat, babel, uglify e browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+// Adiciona os modulos instalados
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+// Funçao para compilar o SASS e adicionar os prefixos
+function compilaSass() {
+  return gulp
+  .src('css/scss/*.scss')
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('css/'))
+  .pipe(browserSync.stream());
+};
+
+// Tarefa de gulp para a função de SASS
+gulp.task('sass', compilaSass);
+
+// Js Plugins
+function pluginsJs(){
+  return gulp.src(['js/plugins/*.js'])
+  .pipe(concat('plugin.js'))
+  .pipe(gulp.dest('js/plugin'))
+  .pipe(browserSync.stream());
+}
+
+gulp.task('pluginsjs', pluginsJs);
+
+// Função para juntar o JS
+function gulpJs(){
+  return gulp.src('js/main/*.js')
+  .pipe(concat('script.js'))
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  .pipe(uglify())
+  .pipe(gulp.dest('js/'))
+  .pipe(browserSync.stream())
+};
+
+gulp.task('mainjs', gulpJs)
+
+// Função para iniciar o browser
+function browser() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+};;;
+
+// Tarefa para iniciar o browser-sync
+gulp.task('browser-sync', browser);
+
+// Função de watch do Gulp
+function watch() {
+  gulp.watch('css/scss/*.scss', compilaSass);
+  gulp.watch('js/main/*.js', gulpJs);
+  gulp.watch('js/plugins/*.js');
+  gulp.watch(['*.html']).on('change', browserSync.reload);
+};
+
+// Inicia a tarefa de watch
+gulp.task('watch', watch);
+
+// Tarefa padrão do Gulp, que inicia o watch e o browser-sync
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'mainjs', 'pluginsjs'));
